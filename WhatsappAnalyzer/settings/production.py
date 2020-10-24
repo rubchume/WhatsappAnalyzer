a9c6a1f4c4cce85dbb632c70fa30747b42b8d0f5
Rubén Chuliá <chanantero@gmail.com>
2020-10-24 11:44:08 +0000

    Multiple settings in settings file for production, local and test environments

---
+++ WhatsappAnalyzer/settings/production.py
@@ -0,0 +1,37 @@
+import django_heroku
+import dj_database_url
+
+from .base import *
+
+DEBUG = False
+
+ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'whatsappanalyzer',
+        'USER': 'name',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}
+
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+django_heroku.settings(locals())
--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'WhatsappAnalyzer.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'WhatsappAnalyzer.settings.base')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
